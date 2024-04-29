99d20daa3bad8e1b2b3b54fabd2322adeff36aeb
Z. Elliott <ellio631@umn.edu>
2024-04-29 17:22:04 -0500

    Edited our main code, by incoporating Wes's changes and my own

--- PaperPlane.m
+++ PaperPlane.m
@@ -1,29 +1,12 @@
 %	Example 1.3-1 Paper Airplane Flight Path
 %	Copyright 2005 by Robert Stengel
 %	August 23, 2005
+% AEM 3103 Final Project, Zachary Elliott & Wesley Girardi
 
-	global CL CD S m g rho	
-	S		=	0.017;			% Reference Area, m^2
-	AR		=	0.86;			% Wing Aspect Ratio
-	e		=	0.9;			% Oswald Efficiency Factor;
-	m		=	0.003;			% Mass, kg
-	g		=	9.8;			% Gravitational acceleration, m/s^2
-	rho		=	1.225;			% Air density at Sea Level, kg/m^3	
-	CLa		=	3.141592 * AR/(1 + sqrt(1 + (AR / 2)^2));
-							% Lift-Coefficient Slope, per rad
-	CDo		=	0.02;			% Zero-Lift Drag Coefficient
-	epsilon	=	1 / (3.141592 * e * AR);% Induced Drag Factor	
-	CL		=	sqrt(CDo / epsilon);	% CL for Maximum Lift/Drag Ratio
-	CD		=	CDo + epsilon * CL^2;	% Corresponding CD
-	LDmax	=	CL / CD;			% Maximum Lift/Drag Ratio
-	Gam		=	-atan(1 / LDmax);	% Corresponding Flight Path Angle, rad
-	V		=	sqrt(2 * m * g /(rho * S * (CL * cos(Gam) - CD * sin(Gam))));
-							% Corresponding Velocity, m/s
-	Alpha	=	CL / CLa;			% Corresponding Angle of Attack, rad
+	
 	
 %	a) Equilibrium Glide at Maximum Lift/Drag Ratio
-	H		=	2;			% Initial Height, m
-	R		=	0;			% Initial Range, m
+    [V Gam H R] = setup_sim();
 	to		=	0;			% Initial Time, sec
 	tf		=	6;			% Final Time, sec
 	tspan	=	[to tf];
@@ -73,7 +56,7 @@
 	plot(xa(:,4),xa(:,3),xb(:,4),xb(:,3),xc(:,4),xc(:,3),xd(:,4),xd(:,3))
 	xlabel('Range, m'), ylabel('Height, m'), grid
 
-	figure(3)
+	figure
 	subplot(2,2,1)
 	plot(ta,xa(:,1),tb,xb(:,1),tc,xc(:,1),td,xd(:,1))
 	xlabel('Time, s'), ylabel('Velocity, m/s'), grid
